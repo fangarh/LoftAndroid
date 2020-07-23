c2004f653ce17fad0ea5fe3f4d193cdac92790d2
Dmitry Dvornikov <dvornikov.d@gmail.com>
2020-07-23 16:57:46 +0300

    task2 started

---
+++ app/src/main/java/com/dds/loftmoney/objects/BudgetRow.java
@@ -0,0 +1,37 @@
+package com.dds.loftmoney.objects;
+
+public class BudgetRow {
+    private String _name;
+    private String _price;
+    private Integer _color;
+
+    public String get_name() {
+        return _name;
+    }
+
+    public void setName(String name) {
+        _name = name;
+    }
+
+    public String getPrice() {
+        return _price;
+    }
+
+    public void setPrice(String price) {
+        _price = price;
+    }
+
+    public Integer getColor() {
+        return _color;
+    }
+
+    public void setColor(Integer color) {
+        _color = color;
+    }
+
+    public BudgetRow(String name, String price, Integer color) {
+        _name = name;
+        _price = price;
+        _color = color;
+    }
+}
