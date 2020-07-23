4ea581538a6da5c602574acf75f4843051cf3e2a
Dmitry Dvornikov <dvornikov.d@gmail.com>
2020-07-23 16:20:34 +0300

    some refactor

--- app/src/main/java/com/dds/loftmoney/AddItemActivity.java
+++ app/src/main/java/com/dds/loftmoney/AddItemActivity.java
@@ -2,13 +2,25 @@ package com.dds.loftmoney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddItemActivity extends AppCompatActivity {
+    private void initListeners(){
+        findViewById(R.id.addItemAddBtn).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent newActivity = new Intent(getApplicationContext(), MainActivity.class);
 
+                startActivity(newActivity);
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        initListeners();
     }
 }
